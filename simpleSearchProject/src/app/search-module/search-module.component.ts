4408e30e878fa26d94dcaa93d6297cd4c690c112
KonradK4698 <93993151+KonradK4698@users.noreply.github.com>
2022-06-02 23:31:56 +0200

    create simple input search bar

--- simpleSearchProject/src/app/search-module/search-module.component.ts
+++ simpleSearchProject/src/app/search-module/search-module.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-search-module',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchModuleComponent implements OnInit {
 
+  searchBar = new FormControl('');
+
   constructor() { }
 
   ngOnInit(): void {
+
+    console.log(this.searchBar.valueChanges.subscribe({
+      next: getVal => console.log(getVal)
+    }))
+
   }
+
+  
 
 }
