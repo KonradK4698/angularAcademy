f343b45d98c3c388fb6fb72ccad2f0e61f60e27e
KonradK4698 <93993151+KonradK4698@users.noreply.github.com>
2022-06-03 12:04:54 +0200

    prepare service to get phrases

--- simpleSearchProject/src/app/search.service.ts
+++ simpleSearchProject/src/app/search.service.ts
@@ -1,10 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, tap, of, catchError } from 'rxjs';
 import {HttpClient, HttpHeaders } from '@angular/common/http'
-
-interface Phrase {
-  phrase: string;
-}
+import { Phrase } from './phraseInterface';
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +12,21 @@ export class SearchService {
   private searchPath = '/api/phrases'
 
   constructor(private http: HttpClient) { }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      return of(result as T);
+    };
+  }
   
-  findPhrases(): Observable<Phrase[]>{
-    return this.http.get<Phrase[]>(this.searchPath);
+  findPhrases(word: string): Observable<Phrase[]>{
+    if (!word.trim()) {
+      return of([]);
+    }
+    return this.http.get<Phrase[]>(`${this.searchPath}/?phrase=${word}`).pipe(
+      tap(phrase => phrase.length ? console.log("Matching phrases found") : console.log("No matching phrases")),
+      catchError(this.handleError<Phrase[]>('searchPhrase', []))
+    );
   }
 }
